a68f6fe7fee1db8b1022e8a051a8f98d40f91094
Shivanandmetri <shivanand1998metri@gmail.com>
2023-07-24 14:59:25 +0530

    Home Page

--- src/Screens/Layouts/Dash.jsx
+++ src/Screens/Layouts/Dash.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 const Dash = () => {
-  return <Outlet />;
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img
+        className="mx-auto h-16 w-auto"
+        src={require("../../assets/bufferzeroLogo.png")}
+        alt="Your Company"
+      />
+    </div>
+    <Outlet />
+  </div>
+  );
 };
 
 export default Dash;
