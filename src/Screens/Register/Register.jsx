2f110005b93ab0aea372284afe28f2e612b887e3
Meghamala KM <meghamala.km@logituit.com>
2023-07-24 13:07:30 +0530

    register commit

--- src/Screens/Register/Register.jsx
+++ src/Screens/Register/Register.jsx
@@ -2,11 +2,7 @@ import React from "react";
 
 const Register = () => {
   return (
-    <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label
@@ -101,19 +97,7 @@ const Register = () => {
               </button>
             </div>
           </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{" "}
-            <a
-              href="#"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              Start a 14 day free trial
-            </a>
-          </p>
         </div>
-      </div>
-    </>
   );
 };
 
