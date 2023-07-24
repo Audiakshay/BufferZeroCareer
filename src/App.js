ec4ab1ff74fa7a2f88df0c0d049be525aba1acbf
Audiakshay <121844200+Audiakshay@users.noreply.github.com>
2023-07-24 12:37:17 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import logo from "./logo.svg";
 import "./index.css";
 import Login from "./Screens/Login/Login";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Auth from "./Screens/Layouts/Auth";
+import Register from "./Screens/Register/Register";
+import Dash from "./Screens/Layouts/Dash";
+import Home from "./Screens/home/home";
 
 function App() {
+  const route = createBrowserRouter([
+    {
+      path: '/',
+      element: <Auth />,
+      children: [
+        {
+          index: true,
+          element: <Login />
+        },
+        {
+          path: 'register',
+          element: <Register />
+        },
+      ]
+    },
+    {
+      path: '/Home',
+      element: <Dash />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        }
+      ]
+    },
+  ])
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <RouterProvider router={route} />
   );
 }
 
